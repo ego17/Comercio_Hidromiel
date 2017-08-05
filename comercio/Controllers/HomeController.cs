518bef8e78d277428657f74a889374eadd1398a2
ego17 <30747081+ego17@users.noreply.github.com>
2017-08-05 04:31:04 -0500

    Avance1
    avance ecommerce

---
+++ comercio/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace comercio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Models.TiendaEntities bd = new Models.TiendaEntities();
+        
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buscar(string id ="")
+        {
+
+            var productos = bd.Producto
+                .Where(x => x.Descripcion.Contains(id))
+                .Take(10)
+                .ToList();
+            ViewBag.clave = id;
+            return View(productos);
+        }
+
+        public ActionResult Producto(string id )
+        {
+            ViewBag.producto = id;
+            return View();
+        }
+
+
+    }
+}
